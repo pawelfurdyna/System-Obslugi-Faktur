65de72cca35a563f3b3b97093abdc08d798a6a3b
Mateusz Drabowicz <matidraba22a@gmail.com>
2023-12-29 14:33:46 +0100

    optymalizacja kodu
    Funkcje dodaj, edytuj.

--- ProjektBD/ObslugaBazy.cs
+++ ProjektBD/ObslugaBazy.cs
@@ -1,5 +1,6 @@
 ﻿using Oracle.ManagedDataAccess.Client;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -19,24 +20,35 @@ namespace ProjektBD
             this.connectionString = ConfigurationManager.ConnectionStrings["ProjektBD.Properties.Settings.ConnectionString"].ConnectionString;
             this.conn = new OracleConnection(connectionString);
         }
-        public void WypelnijTextBoxZEncji(string encja, string klucz, string nazwa, TextBox[] pola, string[] args)
+        #region WypelnijTextBox
+        public void WypelnijTextBoxZEncji(string encja, string klucz, string nazwa, TextBox[] tb, string[] atrybuty)
         {
-            for (int i = 0; i < args.Length; i++)
+            for (int i = 0; i < atrybuty.Length; i++)
             {
-                using (OracleCommand cmd = new OracleCommand($"SELECT {args[i]} FROM {encja} WHERE {klucz} = '{nazwa}'", this.conn))
+                using (OracleCommand cmd = new OracleCommand($"SELECT {atrybuty[i]} FROM {encja} WHERE {klucz} = '{nazwa}'", this.conn))
                 {
-                    this.conn.Open();
-                    OracleDataReader rdr = cmd.ExecuteReader();
-                    if (rdr.Read())
+                    try
                     {
-                        string temp = rdr[args[i]].ToString();
-                        pola[i].Text = temp;
+                        this.conn.Open();
+                        OracleDataReader rdr = cmd.ExecuteReader();
+                        if (rdr.Read())
+                        {
+                            string temp = rdr[atrybuty[i]].ToString();
+                            tb[i].Text = temp;
+                        }
+                        this.conn.Close();
                     }
-                    this.conn.Close();
+                    catch (OracleException ex)
+                    {
+                        MessageBox.Show($"Wystąpił błąd bazy danych. \nError : {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        conn.Close();
+                    }
                 }
             }
         }
+        #endregion
 
+        #region UsunRekord
         public void UsunRekord(string encja, string klucz, string nazwa)
         {
             DialogResult dr = MessageBox.Show($"Czy napewno chcesz usunąć \"{nazwa}\" ?",
@@ -77,15 +89,102 @@ namespace ProjektBD
                     break;
             }
         }
-        public void DodajRekord(string encja, string klucz, string nazwa)
+        #endregion
+
+        #region DodajRekord
+        public void DodajRekord(string encja, TextBox[] tb, string[] atrybuty)
         {
+            string formattedStringAtrybuty = string.Join(", ", atrybuty);
+            string formattedStringTb = "";
+            for (int i = 0; i < tb.Length; i++)
+            {
+                formattedStringTb += tb[i].Text;
+                if (i < tb.Length - 1)
+                {
+                    formattedStringTb += "', '";
+                }
+            }
+            string query = $"INSERT INTO {encja} ({formattedStringAtrybuty}) VALUES ('{formattedStringTb}')";
+            
+            using (this.conn)
+            {
+                try
+                {
+                    this.conn.Open();
 
+                    using (OracleCommand cmd = new OracleCommand(query, this.conn))
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected != 0)
+                        {
+                            if (rowsAffected == 1)
+                            {
+                                MessageBox.Show($"Poprawnie dodano {rowsAffected} rekord!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Poprawnie dodano {rowsAffected} rekordów!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+
+                        this.conn.Close();
+                    }
+                }
+                catch (OracleException ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd bazy danych. \nError : {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.conn.Close();
+                }
+            }
         }
+        #endregion
 
-        public void EdytujRekord(string encja, string klucz, string nazwa)
+        #region EdytujRekord
+        public void EdytujRekord(string encja, string klucz, string nazwa, TextBox[] tb, string[] atrybuty)
         {
+            string formattedString = "";
+            for (int i = 0; i < tb.Length; i++)
+            {
+                formattedString += $"{atrybuty[i]}='{tb[i].Text}'";
+                if (i < tb.Length - 1)
+                {
+                    formattedString += ", ";
+                }
+            }
+            string query = $"UPDATE {encja} SET {formattedString} WHERE {klucz}='{nazwa}'";
 
+            using (this.conn)
+            {
+                try
+                {
+                    this.conn.Open();
+
+                    using (OracleCommand cmd = new OracleCommand(query, this.conn))
+                    {
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected != 0)
+                        {
+                            if (rowsAffected == 1)
+                            {
+                                MessageBox.Show($"Poprawnie edytowano {rowsAffected} rekord!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Poprawnie edytowano {rowsAffected} rekordów!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+
+                        this.conn.Close();
+                    }
+                }
+                catch (OracleException ex)
+                {
+                    MessageBox.Show($"Wystąpił błąd bazy danych. \nError : {ex.Message}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.conn.Close();
+                }
+            }
         }
+        #endregion
     }
 }
 
