016f4e9db836031216a4961f7ea9d5e8adf9222c
Paweł Furdyna <pawelfurdyna@gmail.com>
2023-12-30 16:05:20 +0100

    Aktualizacja
    Przerobienie oraz dodanie funkcjonalności tabeli w FormNowaFaktura

--- ProjektBD/FormNowaFaktura.cs
+++ ProjektBD/FormNowaFaktura.cs
@@ -29,6 +29,8 @@ namespace ProjektBD
 
         private void FormNowaFaktura_Load(object sender, EventArgs e)
         {
+            // TODO: Ten wiersz kodu wczytuje dane do tabeli 'bDdataSet.USLUGA' . Możesz go przenieść lub usunąć.
+            this.uSLUGATableAdapter.Fill(this.bDdataSet.USLUGA);
             // TODO: Ten wiersz kodu wczytuje dane do tabeli 'bDdataSet.KLIENT' . Możesz go przenieść lub usunąć.
             this.kLIENTTableAdapter.Fill(this.bDdataSet.KLIENT);
             // TODO: Ten wiersz kodu wczytuje dane do tabeli 'bDdataSet.POZYCJA_FAKTURY' . Możesz go przenieść lub usunąć.
@@ -38,7 +40,30 @@ namespace ProjektBD
             ObslugaBazy ob = new ObslugaBazy();
             ob.WypelnijComboBoxZEncji("UZYTKOWNIK",cbUzytkownik,new string[] { "ID_UZYTKOWNIKA","IMIE","NAZWISKO" });
 
+            #region DataGrid
+            // Remove the existing 'usluga' column
+            dataGridView1.Columns.Remove("usluga");
 
+            // Create a new DataGridViewComboBoxColumn
+            DataGridViewComboBoxColumn uslugaColumn = new DataGridViewComboBoxColumn
+            {
+                Name = "usluga",
+                HeaderText = "Usługa",
+                DataSource = uSLUGABindingSource,
+                DataPropertyName = "NAZWA", // Bind this column to the 'NAZWA' property
+                DisplayMember = "NAZWA",
+                ValueMember = "NAZWA",
+                AutoComplete = true
+            };
+
+            // Add the new column to the DataGridView
+            dataGridView1.Columns.Insert(1, uslugaColumn);
+
+
+
+
+
+            #endregion
 
             string temp = ob.Select("FAKTURA", "NUMER_FAKTURY", "", "", false);
             if (int.TryParse(temp, out int number))
@@ -52,12 +77,9 @@ namespace ProjektBD
                 tbNrFaktury.Enabled = true;
             }
 
-
-
             tbDataWystawienia.Text = DateTime.Today.ToString("dd-MM-yyyy");
             tbDataWykonaniaUslugi.Text = DateTime.Today.ToString("dd-MM-yyyy");
             tbMiejsceWystawienia.Text = ob.Select("FIRMA", "MIEJSCOWOSC", "", "", false);
-            //sporzadził ma sie wypełniać z uzytkownika
         }
 
         private void cbKlient_SelectedIndexChanged(object sender, EventArgs e)
@@ -90,11 +112,6 @@ namespace ProjektBD
 
         }
 
-        private void cbUzytkownik_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
         private void tbNrFaktury_KeyPress(object sender, KeyPressEventArgs e)
         {
             ob.SprawdzTyp(sender, e);
@@ -123,6 +140,44 @@ namespace ProjektBD
         private void tbDataWykonaniaUslugi_Validating(object sender, CancelEventArgs e)
         {
             ob.WalidacjaDaty(sender, e);
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdytuj_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsun_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellStateChanged(object sender, DataGridViewCellStateChangedEventArgs e)
+        {
+            if (dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.ColumnIndex == dataGridView1.Columns["usluga"]?.Index)
+            {
+                var uslugaCell = dataGridView1.CurrentCell as DataGridViewComboBoxCell;
+                var currentRowIndex = dataGridView1.CurrentCell.RowIndex;
+                var jmCell = dataGridView1.Rows[currentRowIndex].Cells["jm"];
+                var cenaJednostkowaCell = dataGridView1.Rows[currentRowIndex].Cells["cenaJednostkowa"];
+
+                if (uslugaCell != null && uslugaCell.Value != null && jmCell != null && cenaJednostkowaCell != null)
+                {
+                    string selectedUsluga = uslugaCell.Value.ToString();
+
+                    string jmValue = ob.Select("USLUGA", "JEDNOSTKA_MIARY", "NAZWA", selectedUsluga);
+                    string cenaJednostkowaValue = ob.Select("USLUGA", "CENA_JEDNOSTKOWA", "NAZWA", selectedUsluga);
+
+                    // Update the cells
+                    jmCell.Value = jmValue;
+                    cenaJednostkowaCell.Value = cenaJednostkowaValue;
+                }
+            }
         }
     }
 }
